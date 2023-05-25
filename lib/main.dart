f022c7a8575a4de90cf2bdb63e2ceb81260d1a7c
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-05-24 21:31:09 -0300

    app now prints progress

--- lib/main.dart
+++ lib/main.dart
@@ -164,6 +164,29 @@ class DisplayPictureScreen extends StatelessWidget {
                   .putFile(File(imagePath), metadata);
 
               uploadTask;
+
+              uploadTask.snapshotEvents.listen((TaskSnapshot taskSnapshot) {
+                switch (taskSnapshot.state) {
+                  case TaskState.running:
+                    final progress =
+                        100.0 * (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes);
+                    print("Upload is $progress% complete.");
+                    break;
+                  case TaskState.paused:
+                    print("Upload is paused.");
+                    break;
+                  case TaskState.canceled:
+                    print("Upload was canceled");
+                    break;
+                  case TaskState.error:
+                  // Handle unsuccessful uploads
+                    break;
+                  case TaskState.success:
+                  // Handle successful uploads on complete
+                  // ...
+                    break;
+                }
+              });
             },
             child: Text('Upload to firestore'),
           ),
